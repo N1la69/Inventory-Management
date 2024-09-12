694de36a3ce17c3b6abf69cc63d0300f518eabf5
Nilanjan Saha <nilanjan.s.1920@gmail.com>
2024-09-12 21:20:03 +0530

    next_commits

---
+++ src/state/api.ts
@@ -0,0 +1,12 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+
+import { fetchBaseQuery, createApi } from "@reduxjs/toolkit/query";
+
+export const api = createApi({
+  baseQuery: fetchBaseQuery({ baseUrl: process.env.NEXT_PUBLIC_API_BASE_URL }),
+  reducerPath: "api",
+  tagTypes: [],
+  endpoints: (build) => ({}),
+});
+
+export const {} = api;
